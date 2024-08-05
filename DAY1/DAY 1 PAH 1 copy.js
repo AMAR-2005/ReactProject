6eb2f38c079fe80aab71a478bd7860c105d12d92
AMAR-2005 <amar.govindaraj@gmail.com>
2024-08-05 18:57:21 +0530

    added file

---
+++ DAY1/DAY 1 PAH 1 copy.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+import './App.css';
+
+ const App=()=>{
+
+ }
+ const Team=()=>{
+  return(
+    <div>
+      <p>This is Team Functional Component</p>
+    </div>
+  )
+ }
+export default Team;
